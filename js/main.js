067b817e0b3cb24a48aa945b46055b7bc55fff85
25takayama <k-takayama@g.ecc.u-tokyo.ac.jp>
2026-03-16 17:35:59 +0900

    hoshosansodanmaecommit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function($){
+    $('.interview_slider').slick({
+        arrows: false,
+        autoplay:true,
+        autoplaySpeed: 5000,  
+        pauseOnHover: true,  
+        dots:true,
+    });
+    $('.gallary_slider').slick({
+        arrows: false,
+        autoplay:true,
+        autoplaySpeed: 5000,  
+        pauseOnHover: true,  
+        dots:true,
+    });
+});
